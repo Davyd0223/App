ce0b6c40bd9725f4e3329fa256326067a6fdec87
FilinSky <daniel.filinski@gmail.com>
2024-09-30 13:39:43 +0300

    Доработка вёрстки

--- App.js
+++ App.js
@@ -1,36 +1,41 @@
-import { StyleSheet, View, TextInput, Image, TouchableOpacity } from 'react-native';
+import {StyleSheet, View, TextInput, Image, TouchableOpacity, SafeAreaView} from 'react-native';
 import Plan from './components/Plan';
 
 
 export default function App() {
+
+
   return (
+
     <View style={styles.screen}>
-      <View style={styles.search}>
-       <View style={styles.input}>
-        <Image style={styles.icon} source={require('./assets/ser.png')}/>
-        <TextInput onTextInput={'ghbtdn'}/>
-       </View>
-        <TouchableOpacity style={styles.setting}>
-          <Image source={require('./assets/Hicon 3/Outline/Filter 5.png')}/>
-        </TouchableOpacity>
-      </View>
-      <Plan/>
+        <View style={styles.search}>
+          <View style={styles.input}>
+            <Image style={styles.icon} source={require('./assets/ser.png')}/>
+
+            <TextInput style={styles.ti} placeholder="Поиск" onTextInput={'ghbtdn'}/>
+
+          </View>
+          <TouchableOpacity style={styles.setting}>
+            <Image source={require('./assets/Hicon 3/Outline/Filter 5.png')}/>
+          </TouchableOpacity>
+        </View>
+        <Plan/>
         <View style={styles.buttonicon}>
-        <TouchableOpacity>
-          <Image source={require('./assets/Active 2/Inactive=Off.png')}/>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image source={require('./assets/Active 4/Inactive=Off.png')}/>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image source={require('./assets/Active 6/Inactive=Off.png')}/>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image source={require('./assets/Active 7/Inactive=On.png')}/>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image source={require('./assets/Active 10/Inactive=Off.png')}/>
-        </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('./assets/Active 2/Inactive=Off.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('./assets/Active 4/Inactive=Off.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('./assets/Active 6/Inactive=Off.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('./assets/Active 7/Inactive=On.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('./assets/Active 10/Inactive=Off.png')}/>
+          </TouchableOpacity>
         </View>
     </View>
   )
@@ -39,25 +44,36 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    paddingHorizontal: 16,
     justifyContent: 'space-between',
     alignItems: 'center',
   },
   icon: {
-    justifyContent: "flex-end",
+    width: 18,
+    height: 18
   },
   search: {
     flexDirection: 'row',
     alignItems: 'center',
-    paddingTop: 56,
-    paddingLeft: 16,
+    justifyContent: 'space-between',
+    columnGap: 16,
+    width: '100%',
+    paddingTop: 54,
   },
   input: {
+    flexDirection: 'row',
+    paddingHorizontal: 8,
+    alignItems: 'center',
+    columnGap: 10,
     fontSize: 17,
-    width: 287,
+    flex: 1,
     height: 38,
-    marginRight: 12,
     borderRadius: 12,
     backgroundColor: '#F3F3F3',
+  },
+  ti: {
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   setting: {
     justifyContent: 'center',
@@ -72,7 +88,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     alignItems: 'center',
     backgroundColor: '#435861',
-    width: 375,
+    width: '100%',
     height: 76,
     borderTopEndRadius: 16,
     borderTopStartRadius: 16,
--- components/Plan.js
+++ components/Plan.js
@@ -1,16 +1,20 @@
 import { StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
+import {useState} from "react";
 
-export default function Plan() {
+export default function Plan()  {
+    const arr = [1, 2, 3, 4, 5, 6, 7]
+    const [activeIndex, setActiveIndex] = useState(3)
+
     return(
         <View style={styles.screen}>
                     <View style={{justifyContent: 'space-around'}}>
-                            <Text style={{fontSize: 19}}> План чтения </Text>
+                            <Text style={{fontSize: 20}}> План чтения </Text>
                         <View style={styles.switch}>
-                            <TouchableOpacity style={styles.switchActive}> Всемирный план </TouchableOpacity>
-                            <TouchableOpacity style={styles.switchPassive}> Мои планы </TouchableOpacity>
+                            <TouchableOpacity style={styles.switchActive}><Text> Всемирный план </Text></TouchableOpacity>
+                            <TouchableOpacity style={styles.switchPassive}><Text> Мои планы </Text></TouchableOpacity>
                         </View>
                     </View>
-                    <View>
+                    <View style={{rowGap: activeIndex}}>
                       <Text style={{fontSize: 13, color: '#898C93'}}> Воскресенье </Text>
                       <Text styles={{fontSize: 14}}> Июль 12. 2023</Text>
                       <Text styles={{fontSize: 17}}> 1-я Паралипоменон 6 глава</Text>
@@ -19,19 +23,35 @@ export default function Plan() {
                       <TouchableOpacity style={{width: 24, height: 24, alignItems: 'center', justifyContent: 'center'}}>
                        <Image source={require('../assets/Vector2.png')}/>
                       </TouchableOpacity>
-                       <TouchableOpacity style={styles.passiveSlider}> 11 </TouchableOpacity>
-                       <TouchableOpacity style={styles.passiveSlider}> 12 </TouchableOpacity>
-                       <TouchableOpacity style={styles.passiveSlider}> 13 </TouchableOpacity>
-                       <TouchableOpacity style={styles.activevSlider}> 14 </TouchableOpacity>
-                       <TouchableOpacity style={styles.passiveSlider}> 15 </TouchableOpacity>
-                       <TouchableOpacity style={styles.passiveSlider}> 16 </TouchableOpacity>
-                       <TouchableOpacity style={styles.passiveSlider}> 17 </TouchableOpacity>
+
+                        {
+                            arr.map((item, index) => {
+                                return (
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            setActiveIndex(index)
+                                        }}
+                                        style={index === activeIndex ? styles.activevSlider : styles.passiveSlider }
+                                    >
+                                        <Text>{item}</Text>
+                                    </TouchableOpacity>
+                                )
+
+                            })
+                        }
+
+                       {/*<TouchableOpacity style={styles.passiveSlider}><Text> 12 </Text></TouchableOpacity>*/}
+                       {/*<TouchableOpacity style={styles.passiveSlider}><Text> 13 </Text></TouchableOpacity>*/}
+                       {/*<TouchableOpacity style={styles.activevSlider}><Text> 14 </Text></TouchableOpacity>*/}
+                       {/*<TouchableOpacity style={styles.passiveSlider}><Text> 15 </Text></TouchableOpacity>*/}
+                       {/*<TouchableOpacity style={styles.passiveSlider}><Text> 16 </Text></TouchableOpacity>*/}
+                       {/*<TouchableOpacity style={styles.passiveSlider}><Text> 17 </Text></TouchableOpacity>*/}
                       <TouchableOpacity style={{width: 24, height: 24, alignItems: 'center', justifyContent: 'center'}}>
                        <Image source={require('../assets/Vector.png')}/>
                       </TouchableOpacity>
                     </View>
                     <View style={{alignItems: 'center'}}>
-                      <TouchableOpacity style={styles.button}> Читать главу по плану </TouchableOpacity>
+                      <TouchableOpacity style={styles.button}><Text> Читать главу по плану </Text></TouchableOpacity>
                     </View>
         </View>
     )
@@ -41,8 +61,7 @@ const styles = StyleSheet.create ({
     screen: {
         flexDirection: 'column',
         justifyContent: "space-between",
-        width: 343,
-        height: 272,
+        width: '100%',
         borderRadius: 12,
         padding: 12,
         backgroundColor: "#F3F3F3",
@@ -57,7 +76,7 @@ const styles = StyleSheet.create ({
     switchActive: {
         alignItems: 'center',
         justifyContent: 'center',
-        width: 156.5,
+        width: '50%',
         height: 26,
         borderWidth: 1,
         borderColor: '#D9D9D9',
@@ -75,7 +94,7 @@ const styles = StyleSheet.create ({
       flexDirection: 'row',
       justifyContent: 'space-around',
       alignItems: 'center',
-      width: 319,
+      width: '100%',
       height: 40,
       fontSize: 16,
     },
@@ -101,7 +120,6 @@ const styles = StyleSheet.create ({
         height: 44,
         borderRadius: 12,
         borderWidth: 1,
-        borderColor: 'black',
         borderColor: '#212224',
     }
 });
